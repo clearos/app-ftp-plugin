5ea9758b4f8b6ac61a200ed7989ea17bf6ffd621
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:39:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'นโยบายเซิร์ฟเวอร์ FTP ให้การควบคุมการเข้าถึงสำหรับแอปพลิเค FTP เซิร์ฟเวอร์';
+$lang['ftp_plugin_app_name'] = 'นโยบาย FTP เซิร์ฟเวอร์';
+$lang['ftp_plugin_ftp_server_user'] = 'ผู้ใช้ FTP เซิร์ฟเวอร์';
