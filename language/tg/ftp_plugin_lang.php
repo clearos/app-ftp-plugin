a912a4710e854b887c17d96f76f2647e8a984874
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:39:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'Сиёсати Server FTP таъмини идоракунии дастрасӣ, барои барнома FTP сервер.';
+$lang['ftp_plugin_app_name'] = 'Сиёсати Server FTP';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP сервер Корбар';
