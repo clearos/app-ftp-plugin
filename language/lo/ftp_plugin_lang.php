dd03cfd88a550bf02e4924055d746910cec2387d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:34:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP ນະໂຍບາຍ Server ໃຫ້ການຄວບຄຸມການເຂົ້າເຖິງສໍາລັບ app FTP Server.';
+$lang['ftp_plugin_app_name'] = 'ນະໂຍບາຍ Server FTP';
+$lang['ftp_plugin_ftp_server_user'] = 'User Server FTP';
