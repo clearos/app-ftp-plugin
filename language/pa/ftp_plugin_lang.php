dc76cdb3561d67890a617444ae94fac0a8378d99
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:36:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP ਸਰਵਰ ਡਰਾਇਰ FTP ਸਰਵਰ ਐਪ ਲਈ ਪਹੁੰਚ ਕੰਟਰੋਲ ਮੁਹੱਈਆ ਕਰਦਾ ਹੈ.';
+$lang['ftp_plugin_app_name'] = 'FTP ਸਰਵਰ ਡਰਾਇਰ';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP ਸਰਵਰ ਯੂਜ਼ਰ';
