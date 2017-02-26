3a09392a5bf32be8bb36ddb675c0ddefd1e179a0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:39:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP Patakaran Server magbigay ng access control para sa app FTP Server.';
+$lang['ftp_plugin_app_name'] = 'FTP Server Patakaran';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server User';
