b7fb460fe6ec3144a1ddbeb356ab88f289c8c014
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:40:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP Izinqubomgomo Server ukunikeza control ukufinyelela lokusebenza FTP Server.';
+$lang['ftp_plugin_app_name'] = 'Izinqubomgomo Server FTP';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server Umsebenzisi';
