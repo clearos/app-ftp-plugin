c010859d33a0366bf6ab073915f68e9cd0bd9882
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:38:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP Server Sera kutoa upatikanaji wa kudhibiti kwa programu FTP Server.';
+$lang['ftp_plugin_app_name'] = 'Sera FTP Server';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server mtumiaji';
