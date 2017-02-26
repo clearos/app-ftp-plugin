3fcd037093fe7c047eef2933ebd1bb0d613218cd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:40:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP סערווירער פּאַלאַסיז צושטעלן צוטריט קאָנטראָל פֿאַר די FTP סערווירער אַפּ.';
+$lang['ftp_plugin_app_name'] = 'FTP סערווירער פּאָליסיעס';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP סערווירער באַניצער';
