8c62774ca9b818ae1fb52a0cdf72e1ae6b7335e6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:33:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP Server Atumatu-enye ohere akara maka FTP Server na-eji ngwa.';
+$lang['ftp_plugin_app_name'] = 'FTP Server Atumatu';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server User';
