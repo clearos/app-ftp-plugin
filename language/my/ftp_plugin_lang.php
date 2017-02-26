8b6fcf18dbdddcadcc0757e80c1eea3a52ff0f16
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:30:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'တယောက်က FTP Server ကိုမူဝါဒများအတယောက်က FTP Server ကို app ကိုများအတွက်ဝင်ရောက်ခွင့်ကိုထိန်းချုပ်ပေး။';
+$lang['ftp_plugin_app_name'] = 'တယောက်က FTP Server ကိုမူဝါဒများ';
+$lang['ftp_plugin_ftp_server_user'] = 'တယောက်က FTP Server ကိုအသုံးပြုသူတို့၏';
