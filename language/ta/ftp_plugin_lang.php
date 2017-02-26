941b05f487efcd5e50b0115e672a7e3ce30bfa44
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:39:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP சர்வர் கொள்கைகள் FTP சர்வர் பயன்பாட்டை கட்டுப்படுத்த உதவும்.';
+$lang['ftp_plugin_app_name'] = 'FTP சர்வர் கொள்கைகள்';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP சர்வர் பயனர்';
