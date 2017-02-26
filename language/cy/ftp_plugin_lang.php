7a0e897572b1a3fdb1e11f575840d1290c09b836
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:40:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'Polisïau Gweinydd FTP darparu rheolaeth mynediad ar gyfer y app Server FTP.';
+$lang['ftp_plugin_app_name'] = 'Polisïau Gweinydd FTP';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Gweinyddwr Defnyddiwr';
