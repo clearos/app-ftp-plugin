60adefbb75c4a49abdff3376dbb84701925451c3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:31:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP palvelin säännöt tarjoavat kulunvalvonta FTP-palvelimen sovellus.';
+$lang['ftp_plugin_app_name'] = 'FTP palvelin säännöt';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP palvelin Käyttäjä';
