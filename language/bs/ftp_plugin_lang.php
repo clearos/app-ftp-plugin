4530990c828e26f0c6cb3f24d59f48ed987242d4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:30:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP Server politike osigurati kontrolu pristupa za FTP server aplikaciju.';
+$lang['ftp_plugin_app_name'] = 'FTP Server politike';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server User';
