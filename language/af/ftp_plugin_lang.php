500bd433f5323b34baa9214bcef8586e2280b0db
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:29:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP-bediener Beleid voorsien toegangsbeheer vir die FTP-bediener app.';
+$lang['ftp_plugin_app_name'] = 'Server Beleid FTP';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server User';
