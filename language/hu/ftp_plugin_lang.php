79cb3e755de327afb45f7b1d2763aa3330f8f7cd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:33:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP szerver házirendje eléréskorlátozást az FTP szerver alkalmazást.';
+$lang['ftp_plugin_app_name'] = 'FTP szerver házirendje';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP szerver felhasználói';
