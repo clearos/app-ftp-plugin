42b553fc427095e12990223d579abcd9f0af4346
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:34:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'ម៉ាស៊ីនបម្រើ FTP ដែលបានផ្តល់នូវគោលការត្រួតពិនិត្យការចូលដំណើរការសម្រាប់ម៉ាស៊ីនបម្រើ FTP របស់កម្មវិធី។';
+$lang['ftp_plugin_app_name'] = 'ម៉ាស៊ីនបម្រើ FTP របស់គោលនយោបាយ';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP របស់អ្នកប្រើម៉ាស៊ីនបម្រើ';
