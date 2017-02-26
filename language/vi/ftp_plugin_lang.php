d82db64cd1e9aebdb08af29e28bf4a97a1eac92b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:40:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP Server Chính sách cung cấp kiểm soát truy cập cho các ứng dụng FTP Server.';
+$lang['ftp_plugin_app_name'] = 'Chính sách FTP Server';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server tài';
