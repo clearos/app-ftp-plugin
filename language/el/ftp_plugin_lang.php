2cc71f3de3480133bf92ae1d7d48d4c2d39f8c3d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:32:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP Πολιτικές διακομιστή παρέχει έλεγχο πρόσβασης για την εφαρμογή FTP Server.';
+$lang['ftp_plugin_app_name'] = 'Πολιτικές διακομιστή FTP';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server χρήστη';
