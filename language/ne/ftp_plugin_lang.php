28e872c4617a8b024f56ef49b7ea3f5d108422fb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:36:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'एफटीपी सर्भर नीतिहरू पनि एफटीपी सर्भर अनुप्रयोगको लागि पहुँच नियन्त्रण प्रदान।';
+$lang['ftp_plugin_app_name'] = 'एफटीपी सर्भर नीतिहरू';
+$lang['ftp_plugin_ftp_server_user'] = 'एफटीपी सर्भर प्रयोगकर्ता';
