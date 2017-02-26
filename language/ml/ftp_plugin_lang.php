3af5b364f6636ab69a5458d42c3cccb564245193
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:35:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP സെർവർ നയങ്ങള് FTP സർവർ അപ്ലിക്കേഷൻ ആക്സസ്സ് നിയന്ത്രണം നൽകുന്നു.';
+$lang['ftp_plugin_app_name'] = 'FTP സെർവർ നയങ്ങള്';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP സെർവർ ഉപയോക്തൃ';
