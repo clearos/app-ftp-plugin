f27ba9f648851b05ad073daffb8617f904a6e9c7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:35:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP Server Policies fanaraha-maso dia manome fahafahana ho an&#39;ny Server App ny FTP.';
+$lang['ftp_plugin_app_name'] = 'FTP Server Policies';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server User';
