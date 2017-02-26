1c17521e33524308c2b23d3c233167352b11b90a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:33:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP Server პოლიტიკის უზრუნველყოს კონტროლის FTP Server app.';
+$lang['ftp_plugin_app_name'] = 'FTP Server პოლიტიკა';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server მომხმარებელი';
