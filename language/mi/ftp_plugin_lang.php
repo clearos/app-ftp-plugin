6390946be9e24ae90d5ff6bc4cc74e584cf20ff7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:35:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'whakarato kaupapa here Tūmau FTP mana uru mo te taupānga Tūmau FTP.';
+$lang['ftp_plugin_app_name'] = 'Kaupapa here Tūmau FTP';
+$lang['ftp_plugin_ftp_server_user'] = 'Kaiwhakamahi Tūmau FTP';
