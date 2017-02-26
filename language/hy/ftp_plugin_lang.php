1941533a2dfd24b18ca4effb96ee5ba2c26a67b1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:29:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP Server քաղաքականությունը տրամադրել մուտքի վերահսկման համար FTP Server հավելվածի:';
+$lang['ftp_plugin_app_name'] = 'FTP Server քաղաքականություն';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server Օգտվող';
