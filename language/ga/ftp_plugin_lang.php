79f196f7822d4176a7f110b0e64d224a5a781b12
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:33:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'a chur ar fáil FTP Polasaithe Server rialaithe rochtana don app FTP Server.';
+$lang['ftp_plugin_app_name'] = 'Polasaithe Server FTP';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server Úsáideora';
