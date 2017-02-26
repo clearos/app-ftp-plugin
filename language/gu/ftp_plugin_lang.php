dfa30b92f36047172a1727d4fda3d8998dcb1c34
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:32:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP સર્વર નીતિઓ FTP સર્વર એપ્લિકેશન માટે ઍક્સેસ નિયંત્રણ પૂરું પાડે છે.';
+$lang['ftp_plugin_app_name'] = 'FTP સર્વર નીતિઓ';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP સર્વર વપરાશકર્તા';
