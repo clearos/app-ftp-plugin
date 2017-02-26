23f4d58677714fd680bba28d0f9770475abfd7cd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:35:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP сервер Бодлого FTP серверийн програм нь хандалтын хяналтыг олгодог.';
+$lang['ftp_plugin_app_name'] = 'FTP сервер бодлого';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP сервер хэрэглэгч';
