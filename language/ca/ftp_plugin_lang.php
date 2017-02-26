f90d4ccc457472d3091ed12b7dd8d023d3a24a18
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:30:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'Condicions del servidor FTP proporcionen control d&#39;accés per a l&#39;aplicació de servidor FTP.';
+$lang['ftp_plugin_app_name'] = 'Condicions del servidor FTP';
+$lang['ftp_plugin_ftp_server_user'] = 'Usuari del servidor FTP';
