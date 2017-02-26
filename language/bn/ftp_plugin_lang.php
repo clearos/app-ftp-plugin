950e46dbd67fbb39ec75f51702a426319f2d7a9c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:30:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP সার্ভারের নীতিসমূহ এফটিপি সার্ভার অ্যাপ্লিকেশন জন্য প্রবেশাধিকার নিয়ন্ত্রণ প্রদান.';
+$lang['ftp_plugin_app_name'] = 'FTP সার্ভারের নীতিসমূহ';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP সার্ভারের ব্যবহারকারী';
