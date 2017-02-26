72fe133230f4e049162b11e0ffffe3b626a6cf07
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:34:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP ಸರ್ವರ್ ನೀತಿಗಳು FTP ಸರ್ವರ್ ಅಪ್ಲಿಕೇಶನ್ ಆಕ್ಸೆಸ್ ಕಂಟ್ರೋಲ್ ನೀಡಲಾಗುತ್ತದೆ.';
+$lang['ftp_plugin_app_name'] = 'FTP ಸರ್ವರ್ ನೀತಿಗಳು';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP ಸರ್ವರ್ ಬಳಕೆದಾರ';
