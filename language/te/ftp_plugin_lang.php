90721c0e4797ff5bafba8331feaa8830943c7caf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:39:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP సర్వర్ విధానాలు FTP సర్వర్ అనువర్తనం ప్రాప్యత నియంత్రణను అందిస్తాయి.';
+$lang['ftp_plugin_app_name'] = 'FTP సర్వర్ విధానాలు';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP సర్వర్ వాడుకరి';
