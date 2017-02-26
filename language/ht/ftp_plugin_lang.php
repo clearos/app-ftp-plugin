8a7d84ef7e2e2e312bb667ef38df56bd8bcd80a1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:32:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'Ftp Règleman sèvè bay aksè kontwòl pou app a ftp sèvè.';
+$lang['ftp_plugin_app_name'] = 'FTP Server Règleman';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server Itilizatè';
