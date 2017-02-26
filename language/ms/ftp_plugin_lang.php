c4e0e8c83f92941c9355f84de77defe0a5b431c7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:36:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP Polisi Server menyediakan kawalan akses untuk aplikasi FTP Server.';
+$lang['ftp_plugin_app_name'] = 'FTP Server Polisi';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server Pengguna';
