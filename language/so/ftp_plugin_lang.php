cb184da7ddd8be352dff9a63e02fe350c064c40e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:38:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'Siyaasadaha Server FTP bixiyaan helitaanka gacanta for app Server FTP ah.';
+$lang['ftp_plugin_app_name'] = 'FTP Siyaasadaha Server';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server User';
