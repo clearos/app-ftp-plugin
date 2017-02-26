e5172528d58dc26e473bfadb1b873933457fae3a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:37:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'ФТП сервер Политике обезбеди контролу приступа за ФТП Сервер апликација.';
+$lang['ftp_plugin_app_name'] = 'ФТП сервер Политике';
+$lang['ftp_plugin_ftp_server_user'] = 'ФТП сервер Корисник';
