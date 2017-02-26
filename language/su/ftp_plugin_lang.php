704e1f70d8579867418f1ca4e3f9f1c8a885d72c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:38:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'Ftp Kawijakan Server nyadiakeun kadali aksés pikeun app ftp Server.';
+$lang['ftp_plugin_app_name'] = 'Ftp Kawijakan Server';
+$lang['ftp_plugin_ftp_server_user'] = 'Ftp Server Pamaké';
