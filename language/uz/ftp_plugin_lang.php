99af137bacd59760c3ee8fc8c5f1dd38954ec9c5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:40:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP Server siyosati FTP Server ilova uchun kirish nazoratini ta&#39;minlash.';
+$lang['ftp_plugin_app_name'] = 'FTP Server siyosati';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server Foydalanuvchi';
