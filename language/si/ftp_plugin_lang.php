179731445a5ec09bb69649923a8a850072b0ca8f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:37:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP සේවාදායකය ප්රතිපත්ති තිබෙන FTP සේවාදායකය යෙදුම සඳහා ප්රවේශ පාලක ලබා දෙයි.';
+$lang['ftp_plugin_app_name'] = 'FTP සේවාදායකය ප්රතිපත්ති';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP සේවාදායකය පරිශීලක';
