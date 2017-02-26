0f965012d55751ed54bdd090ab1d3ffc85a433bf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:34:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP сервер саясаты FTP сервер App үшін кіру бақылауды қамтамасыз.';
+$lang['ftp_plugin_app_name'] = 'FTP сервер саясаты';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP сервер Пайдаланушы';
