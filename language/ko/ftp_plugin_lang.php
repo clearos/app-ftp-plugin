3dbe45ff3b78177c91b88f3db5a82f515d71e9e7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:34:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP 서버 정책은 FTP 서버 응용 프로그램에 대한 액세스 제어를 제공합니다.';
+$lang['ftp_plugin_app_name'] = 'FTP 서버 정책';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP 서버 사용';
