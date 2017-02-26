892224124ec998a64640965ca12a7772781af7e1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:32:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/ftp_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['ftp_plugin_app_description'] = 'FTP Server Asiri samar da damar iko ga FTP Server app.';
+$lang['ftp_plugin_app_name'] = 'FTP Server Asiri';
+$lang['ftp_plugin_ftp_server_user'] = 'FTP Server User';
